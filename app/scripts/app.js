c5088674f739035e480bd4d26e0c59a288aab927
sepans <sepans@sepans.com>
2014-08-25 15:12:54 -0400

    more info

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,10 +2,22 @@
   'use strict';
 
   document.addEventListener('polymer-ready', function() {
-    // Perform some behaviour
-    console.log('Polymer is ready to rock!');
+
+  	var connector = document.querySelector('th-connector');
+  	setTimeout(function() {
+  		var url = document.querySelector('#url');
+	  		if(url && !url.value) {
+	  			url.value = 'http://sepans.com/misc/test.csv';
+			  	if(connector) {
+			  		var stacked = document.querySelector('th-stacked-chart');
+			  		var csv = document.querySelector('th-csv');
+			  		console.log(csv);
+			  		if(stacked && csv) {
+			  			connector.connectElements(csv,'chartDataOutput', stacked, 'chartData');
+			  		}
+				}
+		  }
+  	},12000)
   });
 
-// wrap document so it plays nice with other libraries
-// http://www.polymer-project.org/platform/shadow-dom.html#wrappers
 })(wrap(document));
